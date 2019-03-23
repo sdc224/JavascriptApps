194b333e2a02f1e2dc78a02fc99fffb04bf2107e
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-03-23 16:52:15 +0530

    Added hover change in Hello world --practice

--- HelloWorld/js/main.js
+++ HelloWorld/js/main.js
@@ -4,10 +4,18 @@ window.onload = function () {
     document.getElementById("button").onclick = function (evt) {
         sayHello();
     }
-}
 
+    document.getElementById("button").onmouseover = function(evt) {
+        onHover();
+    }
+}
 
 function sayHello() {
     var messageDialog = new Windows.UI.Popups.MessageDialog("Hello, world!", "Alert");
+    messageDialog.showAsync();
+}
+
+function onHover() {
+    var messageDialog = new Windows.UI.Popups.MessageDialog("Mouse hovered", "Mouse");
     messageDialog.showAsync();
 }
