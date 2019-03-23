79ce8eab72ad75f777e3d0a1954fbf16376163ad
Sourodeep Chatterjee <sourodeep224@outlook.com>
2019-03-23 16:41:37 +0530

    Added Hello World

--- HelloWorld/js/main.js
+++ HelloWorld/js/main.js
@@ -1,1 +1,13 @@
 ﻿// Your code here!
+
+window.onload = function () {
+    document.getElementById("button").onclick = function (evt) {
+        sayHello();
+    }
+}
+
+
+function sayHello() {
+    var messageDialog = new Windows.UI.Popups.MessageDialog("Hello, world!", "Alert");
+    messageDialog.showAsync();
+}
